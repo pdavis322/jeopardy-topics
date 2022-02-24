46ba232a6a510e9b499f94d133034d1722a0919e
pdavis322 <26099766+pdavis322@users.noreply.github.com>
2022-02-24 16:34:18 -0500

    added header component

---
+++ feature-4/src/Components/Components.js
@@ -0,0 +1,8 @@
+import Header from "./Header.js";
+
+const Components = () => {
+    return (
+        <Header />
+    );
+};
+export default Components;
---
+++ feature-4/src/Components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <ul class="nav">
+        <a class="home" href="index.html">jeopardy! topics</a>
+        <ul class="links">
+            <li><a href="#">stats</a></li>
+            <li><a href="#">settings</a></li>
+            <li><a href="#">profile</a></li>
+        </ul>
+        </ul>
+    );
+}
+
+export default Header;
