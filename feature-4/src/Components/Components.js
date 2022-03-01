ff86220177943e588ea7ac8a8a60ae126f0058af
pdavis322 <26099766+pdavis322@users.noreply.github.com>
2022-03-01 16:13:45 -0500

    Moved header to common folder

--- feature-4/src/Components/Components.js
+++ feature-4/src/Components/Components.js
@@ -1,10 +1,18 @@
-import Header from "./Home/Header.js";
+import Header from "./Common/Header.js";
+import Topics from "./Home/Topics.js";
+import Clue from "./Home/Clue.js";
+import Answer from "./Home/Answer.js";
 
 export default function Components() {
     return (
         <>
             <Header />
             <hr />
+            <Topics />
+            <div className="content">
+                <Clue />
+                <Answer />
+            </div>
         </>
     );
 };
--- feature-4/src/Components/Home/Topics.js
+++ feature-4/src/Components/Home/Topics.js
@@ -1,13 +1,14 @@
 const Topics = (props) => {
+    // Ternary to show loading text when topics is undefined
     return (
-        props.topics.map((e, i) => {
+        props.topics ? props.topics.map((e, i) => {
             // Ternary to set style of button depending on what the current topic is
             return (
               <button key={i}>
                 {e}
               </button>
             );
-        });
+        }) : <h1 style={{ textAlign: "center" }}>Loading topics...</h1>
     );
 }
 export default Topics;
