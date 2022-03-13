1bbb77a22b9ed93ba3ae1956f7d65e3274ecb129
pdavis322 <26099766+pdavis322@users.noreply.github.com>
2022-03-13 13:36:11 -0400

    Delete environments.js

--- feature-4/src/Services/environments.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    APPLICATION_ID: "E6b9bWtIb60AlktoCK7jhKEbGz4YwHYmhMUaE6KH",
-    JAVASCRIPT_KEY: "593pBi2lBDD0kP3b8yXZER2EEL8i7lAC2lNJj2tD",
-    SERVER_URL: "https://parseapi.back4app.com"
-}
