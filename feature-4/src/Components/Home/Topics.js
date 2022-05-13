461f19c91b1756c601c9df66be68626f4883de4a
pdavis322 <26099766+pdavis322@users.noreply.github.com>
2022-05-13 13:31:52 -0400

    added loading text

--- feature-4/src/Components/Home/Topics.js
+++ feature-4/src/Components/Home/Topics.js
@@ -2,11 +2,14 @@ const Topics = (props) => {
     // Ternary to show loading text when topics is undefined
     return (
         <div className="topic">
-            <div style={{fontSize: "1.5rem", padding: "0.5rem"}}>Topic:</div><select value={props.currentTopic} onChange={props.onTopicChange}>
-                {
-                    props.topics.map((e, i) => {return <option key={i} value={e}>{e}</option>})
-                }
-            </select>
+            {
+                props.topics? 
+                <><div style={{fontSize: "1.5rem", padding: "0.5rem"}}>Topic:</div><select value={props.currentTopic} onChange={props.onTopicChange}>
+                    {
+                        props.topics.map((e, i) => {return <option key={i} value={e}>{e}</option>})
+                    }
+                </select></> : <h2>Loading topics...</h2>
+            }
         </div>
     );
 }
